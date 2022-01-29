6a5b2e8d20430a9ecc37a4f95a40e26645fdec24
Luke Tang <tanlu824@student.otago.ac.nz>
2022-01-29 22:20:17 +1300

    Fixed heavy object bug and renamed objects

--- CopperPrototypes/Assets/Scripts/PlayerThrowing.cs
+++ CopperPrototypes/Assets/Scripts/PlayerThrowing.cs
@@ -51,9 +51,6 @@ public class PlayerThrowing : MonoBehaviour
             mag = Mathf.Clamp(mag, 0.5f, 1.5f);
             Debug.Log("Key held down for " + (Time.time - startTime).ToString("F2") + "s for " + force * mag + " force");
 
-            // Throw object using calculated force
-            currentObject.GetComponent<Interactable>().Throw(firePoint, force * mag);
-
             // Change the state of the player's currently held objects
             if (currentObject.tag == "Sharp")
             {
@@ -72,6 +69,9 @@ public class PlayerThrowing : MonoBehaviour
 
                 bluntObject = null;
             }
+
+            // Throw object using calculated force
+            currentObject.GetComponent<Interactable>().Throw(firePoint, force * mag);
         }
 
         // Right-click near a blunt object to pick it up
